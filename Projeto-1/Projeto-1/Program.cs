518ea6f0dd8e66bc0c1946149a5256fafc97b151
Elyansodre <128232034+Elyansodre@users.noreply.github.com>
2024-04-13 22:38:43 -0300

    Add files via upload

---
+++ Projeto-1/Projeto-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com seu nome completo:");
+                string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+                int casa = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto");
+                double preço = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre seu útimo nome, idade e altura");
+            string[] vet = Console.ReadLine().Split(' ');
+            string UltimoNome = vet[0];
+            int    idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(casa);
+            Console.WriteLine(preço.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(UltimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
